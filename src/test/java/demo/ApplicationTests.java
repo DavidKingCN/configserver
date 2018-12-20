6307c3190ef1e34b648ba5216efb1392a6a1e294
13621151569@yeah.net <13621151569@yeah.net>
2018-12-20 10:24:07 +0800

    调整程序

--- src/test/java/demo/ApplicationTests.java
+++ src/test/java/demo/ApplicationTests.java
@@ -7,24 +7,29 @@ import java.util.Map;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.TestRestTemplate;
+//import org.springframework.boot.test.IntegrationTest;
+//import org.springframework.boot.test.SpringApplicationConfiguration;
+//import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = ConfigServerApplication.class)
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration(classes = ConfigServerApplication.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ConfigServerApplication.class)
 @WebAppConfiguration
-@IntegrationTest("server.port=0")
+//@IntegrationTest("server.port=0")
 public class ApplicationTests {
 
-	@Value("${local.server.port}")
-	private int port = 0;
+//	@Value("${local.server.port}")
+	private int port = 8888;
 
 	@Test
 	public void configurationAvailable() {
